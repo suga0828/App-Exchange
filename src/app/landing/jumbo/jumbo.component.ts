9809300d25c0027518e495293b0e1115576ec9c9
suga0828 <alexander.sf0828@gmail.com>
2018-11-12 12:26:46 -0500

    Calculator adjust

--- src/app/landing/jumbo/jumbo.component.ts
+++ src/app/landing/jumbo/jumbo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef,  Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-jumbo',
@@ -7,32 +7,36 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class JumboComponent implements OnInit {
 
-	@ViewChild('send') send: ElementRef
-	@ViewChild('get') get: ElementRef
-
-	onKey(event: any) {
-		let get = this.get.nativeElement.value
-		let tax = this.send.nativeElement.value
-
-		console.log(`get: ${get}`)
-		console.log(`send: ${tax}`)
-		let taxed = get * (1 - (tax/100) )
-		this.result = event.target.value * taxed;
-	}
-
-	result: number;
-	taxs = [10, 20, 30]
-
-	rates = {
-		USD : '1',
-		COP : '3000',
-		VEF : '200'
-	}
-
-
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
+
+	@ViewChild('tax') tax: ElementRef
+	@ViewChild('rate') rate: ElementRef
+
+  taxs = [10, 20, 30]
+
+  rates = {
+    USD : '1',
+    COP : '3000',
+    VEF : '200'
+  }
+
+  taxN: number = null;
+  rateN: number = null;
+  taxed: number = null;
+  taxedN: number = null;
+  value: number = null;
+  result: number = null;
+
+	onKey(event: any) {
+		this.taxN = this.renderer.selectRootElement(this.tax).nativeElement.value;
+		this.rateN = this.renderer.selectRootElement(this.rate).nativeElement.value;
+    this.taxedN = event.target.value * (this.taxN/100) * this.rateN
+    console.log(`Se cobra una comisión de ${this.taxN}%, se utiliza un tipo de cambio de: ${this.rateN} y se obtiene una ganancia de: ${this.taxedN}`)
+    this.taxed = this.rateN * (1 - (this.taxN/100) )
+    this.result = event.target.value * this.taxed;
+	}
 
 }
