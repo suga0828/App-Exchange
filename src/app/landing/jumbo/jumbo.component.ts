948d2ddfd35d78a9c319a8b211b7b49c5dea7980
suga0828 <alexander.sf0828@gmail.com>
2018-11-17 21:44:51 -0500

    Name changed. Calculator modified and some of responsive

--- src/app/landing/jumbo/jumbo.component.ts
+++ src/app/landing/jumbo/jumbo.component.ts
@@ -9,34 +9,74 @@ export class JumboComponent implements OnInit {
 
   constructor(private renderer: Renderer2) { }
 
+	@ViewChild('send') send: ElementRef
+	@ViewChild('get') get: ElementRef
+
+  senders = [
+    "Skrill (USD)",
+    "Neteller (USD)",
+    "Paypal (USD)",
+    "Payoneer (USD)"
+  ]
+
+  receivers = [
+    "Skrill (USD)",
+    "Neteller (USD)",
+    "Paypal (USD)",
+    "Payoneer (USD)"
+  ]
+
+  pairs = [];
+
   ngOnInit() {
+    for(let i = 0; i < this.senders.length; i++) {
+      for(let n = 0; n < this.receivers.length; n++)
+        this.pairs.push({
+            pair:`${this.senders[i]}-${this.receivers[n]}`,
+            tax:n+10
+          })
+    }
   }
 
-	@ViewChild('tax') tax: ElementRef
-	@ViewChild('rate') rate: ElementRef
+  sendW: number = null;
+  getW: number = null;
+  pair: {};
+  comissionN: number = null;
 
-  taxs = [10, 20, 30]
-
-  rates = {
-    USD : '1',
-    COP : '3000',
-    VEF : '200'
-  }
-
-  taxN: number = null;
-  rateN: number = null;
-  taxed: number = null;
-  taxedN: number = null;
-  value: number = null;
-  result: number = null;
+  tax: number = null;
+  comission: number = null;
+  receiver: number = null;
+  sender: number = null;
 
 	onKey(event: any) {
-		this.taxN = this.renderer.selectRootElement(this.tax).nativeElement.value;
-		this.rateN = this.renderer.selectRootElement(this.rate).nativeElement.value;
-    this.taxedN = event.target.value * (this.taxN/100) * this.rateN
-    console.log(`Se cobra una comisión de ${this.taxN}%, se utiliza un tipo de cambio de: ${this.rateN} y se obtiene una ganancia de: ${this.taxedN}`)
-    this.taxed = this.rateN * (1 - (this.taxN/100) )
-    this.result = event.target.value * this.taxed;
+		this.sendW = this.renderer.selectRootElement(this.send).nativeElement.value;
+		this.getW = this.renderer.selectRootElement(this.get).nativeElement.value;
+    this.pair = `${this.sendW}-${this.getW}`
+    this.tax = this.pairs
+      .find( elems => elems.pair == this.pair )
+      .tax;
+    console.log(`Operación: envía: ${this.sendW}. recibe: ${this.getW}. Se cobra comisión de: ${this.tax}%`)
+    this.comission = 1 - (1 / this.tax)
+    this.comissionN = this.comission * this.tax
+    setTimeout( () => {
+      if (this.sender !== null) {
+        this.receiver = this.sender * this.comission
+        if (this.receiver !== null) {
+        this.sender = this.receiver * this.comission
+        return
+        }
+      }
+
+    }, 1000)
+    // if (this.sender == null || this.receiver == null ) {
+    //     this.sender === 0
+    //     this.receiver === 0
+    // }
+
+    // this.taxedN = event.target.value * (this.taxN/100) * this.rateN
+    // console.log(`Se cobra una comisión de ${this.taxN}%, se utiliza un tipo de cambio de: ${this.rateN} y se obtiene una ganancia de: ${this.taxedN}`)
+    // this.taxed = this.rateN * (1 - (this.taxN/100) )
+    // this.result = event.target.value * this.taxed;
 	}
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import * as M from 'materialize-css/dist/js/materialize';
 
 import { ScrollService } from '../../services/scroll.service';
@@ -10,11 +10,15 @@ import { ScrollService } from '../../services/scroll.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private scrollService: ScrollService) { }
+  constructor(
+    private scrollService: ScrollService,
+    private renderer: Renderer2) { }
 
   ngOnInit() {
-  	let elems = document.querySelectorAll(".sidenav");
-	let instances = M.Sidenav.init(elems);
+
+  	// let elems = document.querySelectorAll(".sidenav");
+	  let elems = this.renderer.selectRootElement('.sidenav')
+    let instances = M.Sidenav.init(elems);
   }
 
   scrollToElement($e) {
