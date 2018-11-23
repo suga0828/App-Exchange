794a9d335d1789816fd3a454567f316f8bd961d8
suga0828 <alexander.sf0828@gmail.com>
2018-11-23 13:44:21 -0500

    Icons changed by Fontawesome icons.

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -11,11 +11,21 @@ import { LandingRoutingModule } from './landing-routing.module';
 import { FormsModule } from '@angular/forms';
 import { TestimonyComponent } from './testimony/testimony.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserCheck } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faGrinAlt,faClock } from '@fortawesome/free-regular-svg-icons';
+
+// Add an icon to the library for convenient access in other components
+library.add(faUserCheck, faThumbsUp, faGrinAlt, faClock);
+
+
 @NgModule({
   imports: [
     CommonModule,
     LandingRoutingModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   declarations: [
   	JumboComponent,
