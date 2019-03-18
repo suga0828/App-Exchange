bc29ea31f2203194a51b0ffbb209805c38fc73bf
QA-Aguayo <aprendiz.aguayo@aguayopublicidad.com>
2019-03-18 13:56:31 -0500

    Renombrar backup de credenciales

---
+++ src/environments/environment.backup.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "your-apiKey",
+    authDomain: "your-authDomain",
+    databaseURL: "your-databaseURL",
+    projectId: "your-projectId",
+    storageBucket: "your-storageBucket",
+    messagingSenderId: "your-messagingSenderId"
+  }
+};
