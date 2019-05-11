fc1686cab6c2830c3a61aa44335bd05c5eb3750e
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-05-11 11:12:59 -0500

    Actualizar script para despliegue

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-# Script utilizado para el proceso de despliegue de la aplicación.
+# Script utilizado para el despliegue de la aplicación
+# utilizando Angular Github Pages
 
+echo "Inicia despliegue"
 rm -rf dist
 echo "Carpeta /dist borrada. Comienzo de la compilación"
 ng build --prod=true --aot=true --buildOptimizer=true --optimization=true --vendorChunk=true --base-href "https://suga0828.github.io/App-Exchange/"
 echo "Aplicación compilada"
-npx angular-cli-ghpages --no-silent --dir=dist/App-Exchange
-echo "Aplicación cargada en la rama gh-pages"
+npx angular-cli-ghpages --dir=dist/App-Exchange
+echo "Aplicación cargada en la rama gh-pages del repositoro"
