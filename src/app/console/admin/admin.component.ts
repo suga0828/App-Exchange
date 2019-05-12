2aac3fc690c38200665e6abf974c15e72fcb1ea4
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-05-11 21:20:24 -0500

    Crear vista de administracion

--- src/app/console/admin/admin.component.ts
+++ src/app/console/admin/admin.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { MatTableDataSource, MatPaginator, MatDialog, MatSnackBar } from '@angular/material';
+import { UserService } from '../../services/user.service';
+
+import { User } from '../../interfaces/user';
+import { Plataform } from 'src/app/interfaces/plataform';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  plataforms: any;
+  plataformsColumns: string[] = ['name', 'tax', 'plataformsOptions'];
+  @ViewChild(MatPaginator) platformsPaginator: MatPaginator;
+  
+  users: any;
+  usersColumns: string[] = ['displayName', 'idDocument', 'idDocumentImage', 'status', 'usersOptions'];
+  @ViewChild(MatPaginator) usersPaginator: MatPaginator;
+
+  newPlataform: string;
+  newComission: number;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.getUsers();
+    this.getPlataforms();
+  }
+
+  getPlataforms() {
+    this.userService.getPlataforms()
+      .subscribe( (plataforms: Plataform[]) => {
+        this.plataforms = new MatTableDataSource(plataforms);
+        this.plataforms.paginator = this.platformsPaginator;
+      });
+  }
+
+  getUsers() {
+    this.userService.getUsers()
+      .subscribe( (users: User[]) => {
+        this.users = new MatTableDataSource(users);
+        this.users.paginator = this.usersPaginator;
+      });
+  }
+
+  registerPlataform() {
+    const id = Date.now();
+    this.userService.registerPlataform('Plataforma', id);
   }
 
 }
