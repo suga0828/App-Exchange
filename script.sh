1f74260e0ca0ebff55e3ec9c54c455b63c150e8c
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-05-19 15:55:42 -0500

    Preparar aplicacion para despliegue con Firebase

--- script.sh
+++ script.sh
@@ -5,7 +5,15 @@
 echo "Inicia despliegue"
 rm -rf dist
 echo "Carpeta /dist borrada. Comienzo de la compilación"
-ng build --prod=true --aot=true --buildOptimizer=true --optimization=true --vendorChunk=true --base-href "https://suga0828.github.io/App-Exchange/"
+# ng build --prod=true --aot=true --buildOptimizer=true --optimization=true --vendorChunk=true --base-href "https://suga0828.github.io/App-Exchange/"
+ng build --prod=true --aot=true --buildOptimizer=true --optimization=true --vendorChunk=true
 echo "Aplicación compilada"
-npx angular-cli-ghpages --dir=dist/App-Exchange
-echo "Aplicación cargada en la rama gh-pages del repositoro"
+
+# npx angular-cli-ghpages --dir=dist/App-Exchange
+# echo "Aplicación cargada en la rama gh-pages del repositoro"
+
+echo "Comienzo del despliegue"
+# Script utilizado para el despliegue de la aplicación
+# utilizando Firebase
+firebase deploy --only hosting:intercambios-sin-fronteras
+echo "Aplicación desplegada"
