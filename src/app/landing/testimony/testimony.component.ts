49634c460026e6ab1bf8adc4729c7d92d46c5eb9
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-05-25 18:35:57 -0500

    Prevenir error del componente Carousel

--- src/app/landing/testimony/testimony.component.ts
+++ src/app/landing/testimony/testimony.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-testimony',
   templateUrl: './testimony.component.html',
   styleUrls: ['./testimony.component.scss']
 })
-export class TestimonyComponent implements OnInit {
+export class TestimonyComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit() { }
+
+  ngAfterViewInit(): void {
+    this.cdr.detectChanges();
+  }
 
 }
